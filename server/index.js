c6f9eee29c8893ca53a7764822ad127928095ab4
aldinhalilovic <aldinhaliovic06@gmail.com>
2023-03-01 21:17:04 +0100

    assets and routes set up

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth";
+import { register } from "./controllers/auth";
 
 // CONFIGURATIONS
 
@@ -39,6 +41,9 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+
+// ROUTES
+app.use("/routes", authRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
